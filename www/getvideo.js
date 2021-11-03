cf826fd7e62e74a894314cb27d44ff260f1e605c
MattisLindmark <64585570+MattisLindmark@users.noreply.github.com>
2021-11-03 16:25:25 +0100

    Update getvideo.js
    lagt till så den läser start från json.

--- www/getvideo.js
+++ www/getvideo.js
@@ -1,7 +1,7 @@
 
 var GlobalData;
 var topg = '<iframe id="kaltura_player" src="https://d38ynedpfya4s8.cloudfront.net/p/296/sp/29600/embedIframeJs/uiconf_id/23449664/partner_id/296?iframeembed=true&playerId=kaltura_player&entry_id=';
-var midd = '&flashvars[mediaProxy.mediaPlayFrom]=5&amp'
+var midd = '&flashvars[mediaProxy.mediaPlayFrom]='; //5&amp'
 var botg = ';flashvars[autoPlay]=true;flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=0_dif1itof" width="400" height="285" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Kaltura Player">';
 
 //var botg = '&flashvars[autoPlay]=true;flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[mediaProxy.mediaPlayFrom]=120&amp;flashvars[mediaProxy.mediaPlayTo]=600&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=0_dif1itof" width="400" height="285" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Kaltura Player">';
@@ -30,7 +30,7 @@ var botg = ';flashvars[autoPlay]=true;flashvars[streamerType]=auto&amp;flashvars
 
         var div = document.createElement("div");
         div.innerHTML = 'Video: ' + data[i].rubrik + ' ' + data[i].info+'</br>';
-        div.innerHTML += topg+data[i].videoid+midd+botg;    
+        div.innerHTML += topg+data[i].videoid+midd+data[i].start+"&amp"+botg;    
         mainContainer.appendChild(div);
 
         mainContainer.appendChild(div);
